14620c03e99216af87f45e38fed13ae1cc741f18
zdraffko <zdraffkom@gmail.com>
2019-10-31 23:40:55 +0200

    Updates

---
+++ precache-manifest.79e617b668e3cd4a0382461467d4b43b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6fd8030fd2da39dfedc72d321dc3237",
+    "url": "/car-crash-virtual-sensor/index.html"
+  },
+  {
+    "revision": "9df6de9f5b31d2746484",
+    "url": "/car-crash-virtual-sensor/static/css/main.17bf0c29.chunk.css"
+  },
+  {
+    "revision": "e77f2ffde0b2a59192f3",
+    "url": "/car-crash-virtual-sensor/static/js/2.9f9f87f1.chunk.js"
+  },
+  {
+    "revision": "9df6de9f5b31d2746484",
+    "url": "/car-crash-virtual-sensor/static/js/main.bbc5bca9.chunk.js"
+  },
+  {
+    "revision": "bbeea698869ecd517e85",
+    "url": "/car-crash-virtual-sensor/static/js/runtime-main.74695420.js"
+  }
+]);
