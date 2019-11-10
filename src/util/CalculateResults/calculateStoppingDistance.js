7d5873005c6f99b1c086a93f0726d35e0cd1dde6
zdraffko <zdravkomb1@gmail.com>
2019-11-10 19:12:27 +0200

    tweaked mobile design

--- src/util/CalculateResults/calculateStoppingDistance.js
+++ src/util/CalculateResults/calculateStoppingDistance.js
@@ -2,9 +2,9 @@ const calculateStoppingDistance = (carSpeed, reactionTime, roadGradient, roadCon
   const speedInMeters = carSpeed / 3.6;
   const reactionDistance = speedInMeters * reactionTime;
   console.log(`reaction distance ${reactionDistance}`);
-  const breakingDistance = Math.abs((speedInMeters ** 2) / ((2 * 9.8) * (roadConditions + roadGradient)));
-  console.log(`breaking distance ${breakingDistance}`);
-  return reactionDistance + breakingDistance;
+  const brakingDistance = Math.abs((speedInMeters ** 2) / ((2 * 9.8) * (roadConditions + roadGradient)));
+  console.log(`breaking distance ${brakingDistance}`);
+  return reactionDistance + brakingDistance;
 };
 
 export default calculateStoppingDistance;
